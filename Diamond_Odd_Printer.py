00b44db2289fdb39b2226396054769b301ec5898
unknown <arixxdelera21@gmail.com>
2023-09-30 19:07:15 +0800

    Pseudocode has been added

--- Diamond_Odd_Printer.py
+++ Diamond_Odd_Printer.py
@@ -1,1 +1,18 @@
 # Delera, Aritz B.
+
+# 1. Diamond Shape:
+# Write a Python function named print_diamond that takes an odd integer n as an argument and prints a diamond shape with a width of n using the * character.
+# For n = 5, the output should be:
+#    *
+#   ***
+#  *****
+#   ***
+#    *
+# Note: If an even number is passed, the function should return "Please provide an odd integer."
+
+
+# Make a DiamondPrinter class that we will be using for the program
+# define the printing process
+# use for loop for upper hand of the diamond
+# use for loop for the middle row of the diamond
+# use for loop for the lower half of the diamond
